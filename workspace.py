55e75b5a4633ffa5fcd7c3b4bdfd8ec7eeae2a0f
BoppreH <boppreh@gmail.com>
2014-04-12 15:09:40 -0300

    Add Project class

--- workspace.py
+++ workspace.py
@@ -1,10 +1,21 @@
 from path import path
 
+class Project(object):
+    def __init__(self, path_):
+        self.path = path_
+        self.name = path_.basename()
+
+    def __repr__(self):
+        return '{} ({})'.format(self.name, self.path)
+
 class Projects(object):
     def __init__(self):
         self.root = path(r'E:\projects')
-        self.dirs = {d.basename(): d for d in self.root.dirs()
-                     if (d / '.git').exists()}
+        self.dirs = {}
+        for d in self.root.dirs():
+            if (d / '.git').exists():
+                project = Project(d)
+                self.dirs[project.name] = project
 
     def __getitem__(self, name):
         return self.dirs[name]
